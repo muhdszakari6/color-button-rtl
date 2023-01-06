90a2b165f6993b318e990d71baabd36b32dcafac
muhdszakari6 <salimzakari6@gmail.com>
2023-01-06 17:10:48 +0100

    Fixed failing tests

--- src/App.js
+++ src/App.js
@@ -2,21 +2,30 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
+export function replaceCamelWithSpaces(color) {
+  return color.replace(/\B([A-Z])\B/g, " $1");
+}
+
 function App() {
-  const [buttonColor, setButtonColor] = useState("red");
+  const [buttonColor, setButtonColor] = useState("MediumVioletRed");
   const [disabled, setDisabled] = useState(false);
 
   const handleClick = () => {
-    setButtonColor(buttonColor === "red" ? "blue" : "red");
+    setButtonColor(
+      buttonColor === "MediumVioletRed" ? "MidnightBlue" : "MediumVioletRed"
+    );
   };
   return (
     <div className="App">
       <button
         disabled={disabled}
         onClick={handleClick}
-        style={{ backgroundColor: disabled ? "gray" : buttonColor }}
+        style={{ backgroundColor: disabled ? "gray" : buttonColor + "" }}
       >
-        Change to {buttonColor === "red" ? "blue" : "red"}
+        Change to
+        {buttonColor === "MediumVioletRed"
+          ? " Midnight Blue"
+          : " Medium Violet Red"}
       </button>
       <input
         value={disabled}
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,29 +1,35 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import React from "react";
-import App from "./App";
+import App, { replaceCamelWithSpaces } from "./App";
 
 test("button has correct initial color", () => {
   render(<App />);
-  const button = screen.getByRole("button", { name: "Change to blue" });
+  const button = screen.getByRole("button", {
+    name: "Change to Midnight Blue",
+  });
   expect(button).toHaveStyle({
-    "background-color": "red",
+    "background-color": "MediumVioletRed",
   });
 });
 
-test("button turns blue when clicked", () => {
+test("button turns Midnight Blue when clicked", () => {
   render(<App />);
-  const button = screen.getByRole("button", { name: "Change to blue" });
+  const button = screen.getByRole("button", {
+    name: "Change to Midnight Blue",
+  });
   fireEvent.click(button);
 
   expect(button).toHaveStyle({
-    "background-color": "blue",
+    "background-color": "MidnightBlue",
   });
-  expect(button).toHaveTextContent("Change to red");
+  expect(button).toHaveTextContent("Change to Medium Violet Red");
 });
 
 test("initial conditions", () => {
   render(<App></App>);
-  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  const colorButton = screen.getByRole("button", {
+    name: "Change to Midnight Blue",
+  });
   expect(colorButton).toBeEnabled();
 
   const checkbox = screen.getByRole("checkbox");
@@ -34,7 +40,7 @@ test("Checkbox should disable button on first click and re-enable button on seco
   render(<App />);
   const checkbox = screen.getByRole("checkbox");
   const colorButton = screen.getByRole("button", {
-    name: "Change to blue",
+    name: "Change to Midnight Blue",
   });
 
   fireEvent.click(checkbox);
@@ -46,13 +52,27 @@ test("Checkbox should disable button on first click and re-enable button on seco
 
 test("Button should be gray when disabled", () => {
   render(<App />);
-  const button = screen.getByRole("button", { name: "Change to blue" });
+  const button = screen.getByRole("button", {
+    name: "Change to Midnight Blue",
+  });
   const checkbox = screen.getByRole("checkbox");
   expect(button).toHaveStyle({
-    backgroundColor: "red",
+    backgroundColor: "Medium Violet Red",
   });
   fireEvent.click(checkbox);
   expect(button).toHaveStyle({
     backgroundColor: "gray",
+  });
+});
+
+describe("spaces before camel-case capital letters", () => {
+  test("Works for no inner capital letters", () => {
+    expect(replaceCamelWithSpaces("red")).toBe("red");
+  });
+  test("Works for one inner capital letters", () => {
+    expect(replaceCamelWithSpaces("MidnightBlue")).toBe("Midnight Blue");
+  });
+  test("Works for multiple inner capital letters", () => {
+    expect(replaceCamelWithSpaces("MediumVioletRed")).toBe("Medium Violet Red");
   });
 });
